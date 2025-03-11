413dffd3e957bfeb127c9c1919b33b8cb2472d56
StefanDelgado <99627352+StefanDelgado@users.noreply.github.com>
2025-03-11 10:36:32 +0800

    CORs

--- api/authenticator.php
+++ api/authenticator.php
@@ -1,6 +1,10 @@
 <?php
 // filepath: /C:/xampp/htdocs/WebDesign_BSITA-2/2nd sem/Joshan_System/HelioHostServer/api/authenticator.php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 include dirname(__DIR__) . '/Settings/db.php';
 
 function authenticate() {
--- api/microservice_user/login.php
+++ api/microservice_user/login.php
@@ -1,10 +1,19 @@
 <?php
 // filepath: /C:/xampp/htdocs/WebDesign_BSITA-2/2nd sem/Joshan_System/HelioHostServer/api/microservice_user/login.php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
+
 include '../../Settings/db.php';
 include '../authenticator.php';
 
 // Authenticate the API key
+authenticate();
 
 $input = json_decode(file_get_contents('php://input'), true);
 
@@ -17,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($result->num_rows > 0) {
         $user = $result->fetch_assoc();
         if (password_verify($password, $user['password'])) {
-            echo json_encode(['message' => 'Login successful']);
+            echo json_encode(['message' => 'Login successful', 'api_id' => $user['api_id']]);
         } else {
             echo json_encode(['message' => 'Invalid username or password']);
         }
