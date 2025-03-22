8e6b3decadafeb2f9b5d647fb5708874bac5b16c
StefanDelgado <99627352+StefanDelgado@users.noreply.github.com>
2025-03-22 11:45:24 +0800

    Create read_suppliers.php

---
+++ StockRoute_Microservice_API/api/microservice_supplier_products/crud/read_suppliers.php
@@ -0,0 +1,38 @@
+<?php
+// filepath: /C:/xampp/htdocs/WebDesign_BSITA-2/2nd sem/Joshan_System/HelioHostServer/api/microservice_supplier_products/crud/read_suppliers.php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
+
+include '../../../Settings/db.php';
+include '../../authenticator.php';
+
+// Authenticate the API key
+//authenticate();
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $sql = "SELECT DISTINCT suppliers.* 
+            FROM suppliers 
+            INNER JOIN products ON suppliers.supplier_id = products.supplier_id";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $suppliers = [];
+        while ($supplier = $result->fetch_assoc()) {
+            $suppliers[] = $supplier;
+        }
+        echo json_encode($suppliers);
+    } else {
+        echo json_encode(['message' => 'No suppliers found']);
+        http_response_code(404);
+    }
+} else {
+    echo json_encode(['message' => 'Invalid request method']);
+    http_response_code(405);
+}
+?>
