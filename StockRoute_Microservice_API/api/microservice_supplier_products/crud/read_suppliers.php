83031dcbba12061b244d4e875174f132ac1f3402
StefanDelgado <99627352+StefanDelgado@users.noreply.github.com>
2025-03-22 12:11:26 +0800

    Update read_suppliers.php

--- StockRoute_Microservice_API/api/microservice_supplier_products/crud/read_suppliers.php
+++ StockRoute_Microservice_API/api/microservice_supplier_products/crud/read_suppliers.php
@@ -13,7 +13,7 @@ include '../../../Settings/db.php';
 include '../../authenticator.php';
 
 // Authenticate the API key
-authenticate();
+//authenticate();
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $sql = "SELECT DISTINCT microservice_users.* 
